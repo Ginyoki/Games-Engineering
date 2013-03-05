073a740d3685867c3539cf2342b566760739ff12
Mark <m.bradshaw@hotmail.co.uk>
2013-03-05 19:48:01 +0000

    Singleton implemented
    Not much else, empty IEntity class.

---
+++ Shooty_Thing/Shooty_Thing/IEntity.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+
+namespace Shooty_Thing
+{
+    interface IEntity
+    {
+        void LoadContent(ContentManager mContentManager);
+        void UnloadContent();
+
+        void Update(GameTime mGameTime);
+
+        void Draw(SpriteBatch mSpriteBatch);
+
+        void Collision(IEntity with);
+
+        Vector2 Position { get; }
+        Rectangle BoundingBox { get; }
+    }
+}
---
+++ Shooty_Thing/Shooty_Thing/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace Shooty_Thing
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        private static Kernel game;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            game = new Kernel();
+            game.Run();
+        }
+    }
+}
